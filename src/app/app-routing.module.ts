363689ec38e9ebd9879d2532a3c025c3b233b5b1
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-15 00:55:00 -0300

    component book delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { CategoryUpdateComponent } from './components/views/category/category-up
 import { BookReadAllComponent } from './components/views/book/book-read-all/book-read-all.component';
 import { BookCreateComponent } from './components/views/book/book-create/book-create.component';
 import { BookUpdateComponent } from './components/views/book/book-update/book-update.component';
+import { BookDeleteComponent } from './components/views/book/book-delete/book-delete.component';
 
 const routes: Routes = [
   {
@@ -41,6 +42,10 @@ const routes: Routes = [
   {
     path: 'categories/:id_cat/books/:id/update',
     component: BookUpdateComponent
+  },
+  {
+    path: 'categories/:id_cat/books/:id/delete',
+    component: BookDeleteComponent
   }
 ];
 
