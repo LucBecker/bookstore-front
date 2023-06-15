018d2bfd931157a6d38c9b63ce971b8383ce6d5a
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-14 23:54:01 -0300

    add validation formsControl

--- src/app/components/views/book/book-create/book-create.component.ts
+++ src/app/components/views/book/book-create/book-create.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
   templateUrl: './book-create.component.html',
   styleUrls: ['./book-create.component.css']
 })
-export class BookCreateComponent {
+export class BookCreateComponent implements OnInit {
 
+  title = new FormControl("",  [Validators.minLength(3)]);
+  author = new FormControl("",  [Validators.minLength(3)]);
+  text = new FormControl("",  [Validators.minLength(10)]);
+
+  constructor(
+    private service: BookService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  getMessage() {
+    if (this.title.invalid) {
+      return "The title field must contain between 3 and 100 characters";
+    }
+    if (this.author.invalid) {
+      return "The title field must contain between 3 and 100 characters";
+    }
+    if (this.text.invalid) {
+      return "The title field must contain between 10 and 100 characters";
+    }
+    return false;
+  }
 }
