728d4a06dc02dd1bb1ae59b87087b1354ea72259
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-14 23:18:20 -0300

    add read all book

--- src/app/components/views/book/book-read-all/book-read-all.component.ts
+++ src/app/components/views/book/book-read-all/book-read-all.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-book-read-all',
@@ -7,11 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookReadAllComponent implements OnInit {
 
+  id_cat: String = ''
+
+  books: Book[] = [];
+
   displayedColumns: string[] = ['id', 'title', 'books', 'actions'];
 
-  constructor() { }
+  constructor(
+    private service: BookService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAllByCategory(this.id_cat).subscribe(resposta => {
+      this.books = resposta;
+    })
   }
 
 }
